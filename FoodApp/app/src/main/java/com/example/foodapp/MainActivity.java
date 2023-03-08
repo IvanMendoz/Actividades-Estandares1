23c30aacf0dc8821aca2b56f76195f753d76c7af
Iván Landaverde <2502882019@mail.utec.edu.sv>
2023-03-08 07:23:34 -0600

    Avanzando la vaina

--- FoodApp/app/src/main/java/com/example/foodapp/MainActivity.java
+++ FoodApp/app/src/main/java/com/example/foodapp/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    ConstraintLayout btnInicial;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInicial = findViewById(R.id.btnInicio);
+        btnInicial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity( new Intent( getApplicationContext(), Principal.class ) );
+            }
+        });
     }
 }
